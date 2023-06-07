0b984c2239395ab530c443a36f6528c6aa976488
abdulkham1d0v <jamshid.ktl.01@gmail.com>
2023-06-07 09:21:52 +0600

    "refactoring"

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-from flask_cors import cross_origin
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+from pymongo import MongoClient
 import os
 import base64
 import face_recognition
 from PIL import Image
 from io import BytesIO
-import pickle
 import numpy as np
 
-def save_embeddings_database(file_path):
-    with open(file_path, 'wb') as file:
-        pickle.dump(embeddings_database, file)
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
 
-def load_embeddings_database(file_path):
-    with open(file_path, 'rb') as file:
-        return pickle.load(file)
+client = MongoClient(MONGO_URI)
+db = client.test
+users = db.users
 
-# Initialize embeddings_database by loading it from a file
-embeddings_database_file = 'embeddings_database.pkl'
-if os.path.exists(embeddings_database_file):
-    embeddings_database = load_embeddings_database(embeddings_database_file)
-else:
-    embeddings_database = {}    
+recognition_threshold = 0.4
 
 def generate_embedding(img):
+    """
+    Generate a face embedding from an image.
+    
+    Args:
+    img: A numpy array representing an image.
+    
+    Returns:
+    A face embedding as a numpy array, or None if no face could be detected in the image.
+    """
     encodings = face_recognition.face_encodings(img)
     if len(encodings) > 0:
         return encodings[0]
@@ -32,36 +35,39 @@ def generate_embedding(img):
         return None
 
 def load_image_from_base64(base64_img):
+    """
+    Load an image from a base64 encoded string.
+    
+    Args:
+    base64_img: A base64 encoded string representing an image.
+    
+    Returns:
+    A numpy array representing the image.
+    """
     img_data = base64.b64decode(base64_img)
     img = Image.open(BytesIO(img_data))
     return np.array(img)
 
-
-def add_person(person_id, front_img_base64, left_img_base64, right_img_base64):
-    front_img = load_image_from_base64(front_img_base64)
-    left_img = load_image_from_base64(left_img_base64)
-    right_img = load_image_from_base64(right_img_base64)
-
-    front_embedding = generate_embedding(front_img)
-    left_embedding = generate_embedding(left_img)
-    right_embedding = generate_embedding(right_img)
-    #print(f"this is front embedding {type(front_embedding)} \n\n this is front embedding {type(left_embedding)} \n\n  this is front embedding {type(right_embedding)} \n\n ")
-
-    if front_embedding is None or left_embedding is None or right_embedding is None:
-        return False
-
-    average_embedding = (front_embedding + left_embedding + right_embedding) / 3
-    embeddings_database[person_id] = average_embedding
-    # Save the updated embeddings_database to a file
-    save_embeddings_database(embeddings_database_file)
-
-    return True
-
 def set_threshold(threshold):
+    """
+    Set the threshold for face recognition.
+    
+    Args:
+    threshold: A float representing the threshold.
+    """
     global recognition_threshold
     recognition_threshold = threshold
 
 def recognize_person(base64_img):
+    """
+    Recognize a person in an image.
+    
+    Args:
+    base64_img: A base64 encoded string representing an image.
+    
+    Returns:
+    The ID of the recognized person, or None if no person could be recognized.
+    """
     unknown_img = load_image_from_base64(base64_img)
     unknown_embedding = generate_embedding(unknown_img)
     if unknown_embedding is None:
@@ -70,7 +76,12 @@ def recognize_person(base64_img):
     min_distance = float("inf")
     best_match = None
 
-    for person_id, known_embedding in embeddings_database.items():
+    for user in users.find({}):   # iterate over all users in the MongoDB 'users' collection
+        if 'embedding' not in user or user['embedding'] is None: 
+            continue
+        known_embedding = np.array(user['embedding'])
+        person_id = user['_id']   # or other field that serves as person ID
+
         distance = face_recognition.face_distance([known_embedding], unknown_embedding)
 
         if distance < min_distance:
@@ -80,33 +91,20 @@ def recognize_person(base64_img):
     return best_match if min_distance <= recognition_threshold else None
 
 
-recognition_threshold = 0.6
-
-# Example usage:
-# set_threshold(0.5)
-# add_person("001", front_img_base64, left_img_base64, right_img_base64)
-# recognized_person_id = recognize_person(unknown_img_base64)
-
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*", "headers": "*"}})
-
-@app.route('/add_person', methods=['POST'])
-@cross_origin()
-def add_person_route():
-    person_id = request.form['person_id']
-    front_img_base64 = request.form['front_img_base64']
-    left_img_base64 = request.form['left_img_base64']
-    right_img_base64 = request.form['right_img_base64']
-    
-    result = add_person(person_id, front_img_base64, left_img_base64, right_img_base64)
-    if result:
-        return jsonify({'status': 'success', 'message': 'Person added successfully'})
-    else:
-        return jsonify({'status': 'failure', 'message': 'Failed to add person'})
 
 @app.route('/set_threshold', methods=['POST'])
 @cross_origin()
 def set_threshold_route():
+    """
+    Route for setting the threshold for face recognition.
+    
+    Expects a form field 'threshold' in the POST request.
+
+    Returns:
+    A JSON object with status and message fields.
+    """
     threshold = float(request.form['threshold'])
     set_threshold(threshold)
     return jsonify({'status': 'success', 'message': f'Threshold set to {threshold}'})
@@ -114,17 +112,43 @@ def set_threshold_route():
 @app.route('/get_embedding', methods=['POST'])
 @cross_origin()
 def get_embedding_route():
-    base64_img = request.form['base64Img']
-    img = load_image_from_base64(base64_img)
-    embedding = generate_embedding(img)
-    if embedding is not None:
-        return jsonify({'status': 'success', 'embedding': embedding.tolist()})
-    else:
-        return jsonify({'status': 'failure', 'message': 'No face detected'})
+    """
+    Route for getting the average face embedding from three images.
+    
+    Expects form fields 'front_img_base64', 'left_img_base64', and 'right_img_base64' in the POST request.
+
+    Returns:
+    A JSON object with status and message fields, and the embedding if successful.
+    """
+    front_img_base64 = request.form['front_img_base64']
+    left_img_base64 = request.form['left_img_base64']
+    right_img_base64 = request.form['right_img_base64']
+    
+    front_img = load_image_from_base64(front_img_base64)
+    left_img = load_image_from_base64(left_img_base64)
+    right_img = load_image_from_base64(right_img_base64)
+
+    front_embedding = generate_embedding(front_img)
+    left_embedding = generate_embedding(left_img)
+    right_embedding = generate_embedding(right_img)
+
+    if front_embedding is None or left_embedding is None or right_embedding is None:
+        return jsonify({'status': 'failure', 'message': 'No face detected in one of the images'})
+
+    average_embedding = (front_embedding + left_embedding + right_embedding) / 3
+    return jsonify({'status': 'success', 'embedding': average_embedding.tolist()})
 
 @app.route('/recognize_person', methods=['POST'])
 @cross_origin()
 def recognize_person_route():
+    """
+    Route for recognizing a person in an image.
+    
+    Expects a form field 'base64Img' in the POST request.
+
+    Returns:
+    A JSON object with status and message fields, and the person_id if a person was recognized.
+    """
     base64_img = request.form['base64Img']
     recognized_person_id = recognize_person(base64_img)
     if recognized_person_id is not None:
@@ -135,6 +159,12 @@ def recognize_person_route():
 @app.route('/', methods=['GET'])
 @cross_origin()
 def get_index():
+    """
+    The default route.
+
+    Returns:
+    A string saying 'Hello World!'.
+    """
     return "Hello World!"
 
 
